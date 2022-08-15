5df59c542885c5d98ced3578260d113e2b59fdb9
laykis <flash6471@naver.com>
2022-08-15 14:50:48 +0900

    make Repositoies

---
+++ src/main/java/WK/Lab/Repository/ContentRepository.java
@@ -0,0 +1,9 @@
+package WK.Lab.Repository;
+
+import WK.Lab.Entity.Content;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ContentRepository extends JpaRepository<Content, Long> {
+}
---
+++ src/main/java/WK/Lab/Repository/PortfolioRepository.java
@@ -0,0 +1,9 @@
+package WK.Lab.Repository;
+
+import WK.Lab.Entity.Portfolio;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PortfolioRepository extends JpaRepository<Portfolio, Long> {
+}
